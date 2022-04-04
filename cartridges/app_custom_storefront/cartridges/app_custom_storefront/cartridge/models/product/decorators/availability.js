25fdd8d56f5ccdfce306d5bc8bb070e869fba296
Vladyslav Makariuk <vadvadm0@gmail.com>
2022-04-04 21:49:59 +0300

    Extend the Product Model

---
+++ cartridges/app_custom_storefront/cartridges/app_custom_storefront/cartridge/models/product/decorators/availability.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var Resource = require('dw/web/Resource');
+
+var base = module.superModule;
+
+/**
+ * Gets items in stock left messages from resources
+ * @param {Object} availabilityModel javascript model in stock available items
+ * @returns {Object} items in stock
+ */
+function getAtsMessage(availabilityModel) {
+    var ATS = {};
+    ATS.messages = [];
+    var inventoryRecord = availabilityModel.inventoryRecord;
+
+    if (inventoryRecord) {
+        ATS.messages.push(
+            Resource.msgf(
+                'label.quantity.in.stock',
+                'common',
+                null,
+                inventoryRecord.ATS.value
+            )
+        );
+    }
+
+    return ATS;
+}
+
+module.exports = function (object, quantity, minOrderQuantity, availabilityModel) {
+    base.call(this, object, quantity, minOrderQuantity, availabilityModel);
+
+    Object.defineProperty(object, 'ats', {
+        enumerable: true,
+        value: getAtsMessage(availabilityModel)
+    });
+};
