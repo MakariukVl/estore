154abc61037157020d250ed79eeecfe0a6e527e6
Vladyslav Makariuk <vadvadm0@gmail.com>
2022-04-05 10:58:22 +0300

    feature/block_3: fixed Hello-Start route & find a purpose

--- cartridges/app_custom_storefront/cartridges/app_custom_storefront/cartridge/controllers/Hello.js
+++ cartridges/app_custom_storefront/cartridges/app_custom_storefront/cartridge/controllers/Hello.js
@@ -3,6 +3,9 @@
 var server = require('server');
 var cache = require('*/cartridge/scripts/middleware/cache');
 
-server.get(cache.applyDefaultCache);
+server.get('Start', cache.applyDefaultCache, function (req, res, next) {
+    res.json({ message: 'Hello World!' });
+    next();
+});
 
 module.exports = server.exports();
