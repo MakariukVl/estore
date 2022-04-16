c6518ea5e52b20f999d3e3ddba00f7861b90cb2a
Vladyslav Makariuk <vadvadm0@gmail.com>
2022-04-16 13:20:55 +0300

    feature/block_8_2: Use a Middleware Chain Event

--- cartridges/app_custom_storefront/cartridges/app_custom_storefront/cartridge/controllers/Newsletter.js
+++ cartridges/app_custom_storefront/cartridges/app_custom_storefront/cartridge/controllers/Newsletter.js
@@ -19,7 +19,6 @@ server.get('Show', server.middleware.https, csrfProtection.generateToken, functi
 });
 
 server.post('Handler', server.middleware.https, csrfProtection.validateAjaxRequest, function (req, res, next) {
-    var Transaction = require('dw/system/Transaction');
     var Resource = require('dw/web/Resource');
     var newsletterForm = server.forms.getForm('newsletter');
 
@@ -29,41 +28,46 @@ server.post('Handler', server.middleware.https, csrfProtection.validateAjaxReque
         newsletterForm.email.value === newsletterForm.emailconfirm.value;
 
     if (newsletterForm.valid) {
-        try {
-            var CustomObjectMgr = require('dw/object/CustomObjectMgr');
-            Transaction.wrap(function () {
-                var co = CustomObjectMgr.createCustomObject('NewsletterSubscription', newsletterForm.email.value);
-                co.custom.firstName = newsletterForm.fname.value;
-                co.custom.lastName = newsletterForm.lname.value;
-            });
-            // Show the success page
-            res.json({
-                success: true,
-                redirectUrl: URLUtils.url('Newsletter-Success').toString()
-            });
-        } catch (e) {
-            var err = e;
-            if (err.javaName === 'MetaDataException') {
-                /* Duplicate primary key on CO: send back message to client-side, but don't log error.
-                This is possible if the user tries to subscribe with the same email multiple times */
-                res.json({
-                    success: false,
-                    error: [Resource.msg('error.subscriptionexists', 'newsletter', null)]
+        // eslint-disable-next-line no-shadow
+        this.on('route:BeforeComplete', function (req, res) {
+            try {
+                var Transaction = require('dw/system/Transaction');
+                var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+
+                Transaction.wrap(function () {
+                    var co = CustomObjectMgr.createCustomObject('NewsletterSubscription', newsletterForm.email.value);
+                    co.custom.firstName = newsletterForm.fname.value;
+                    co.custom.lastName = newsletterForm.lname.value;
                 });
-            } else {
-                /* Missing CO definition: Log error with message for site admin, set the response to error, 
-                and send error page URL to client-side */
-                var Logger = require('dw/system/Logger');
-                Logger.getLogger('newsletter_subscription')
-                    .error(Resource.msg('error.customobjectmissing', 'newsletter', null));
-                // Show general error page: there is nothing else to do
-                res.setStatusCode(500);
+                // Show the success page
                 res.json({
-                    error: true,
-                    redirectUrl: URLUtils.url('Error-Start').toString()
+                    success: true,
+                    redirectUrl: URLUtils.url('Newsletter-Success').toString()
                 });
+            } catch (e) {
+                var err = e;
+                if (err.javaName === 'MetaDataException') {
+                    /* Duplicate primary key on CO: send back message to client-side, but don't log error.
+                    This is possible if the user tries to subscribe with the same email multiple times */
+                    res.json({
+                        success: false,
+                        error: [Resource.msg('error.subscriptionexists', 'newsletter', null)]
+                    });
+                } else {
+                    /* Missing CO definition: Log error with message for site admin, set the response to error, 
+                    and send error page URL to client-side */
+                    var Logger = require('dw/system/Logger');
+                    Logger.getLogger('newsletter_subscription')
+                        .error(Resource.msg('error.customobjectmissing', 'newsletter', null));
+                    // Show general error page: there is nothing else to do
+                    res.setStatusCode(500);
+                    res.json({
+                        error: true,
+                        redirectUrl: URLUtils.url('Error-Start').toString()
+                    });
+                }
             }
-        }
+        });
     } else {
         // Handle server-side validation errors here: this is just an example
         res.json({
